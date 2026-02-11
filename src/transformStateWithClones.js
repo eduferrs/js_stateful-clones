5a6aa89a8b0f5746764fa11e12a89df3a9fa1357
eduferrs <edu.ferr96@gmail.com>
2026-02-11 12:55:25 -0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = Object.assign({}, state);
+  let statesHistory = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(stateCopy, actions[i].extraData);
+        statesHistory.push(Object.assign({}, stateCopy));
+        break;
+      case 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete stateCopy[actions[i].keysToRemove[j]];
+        }
+        statesHistory.push(Object.assign({}, stateCopy));
+        break;
+      case 'clear':
+        stateCopy = {};
+        statesHistory.push(Object.assign({}, stateCopy));
+        break;
+      default:
+        break;
+    }
+  }
+
+  return statesHistory;
 }
 
 module.exports = transformStateWithClones;
