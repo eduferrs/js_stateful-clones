6020e68e575169247b06c77df6f45ae709b6b2d0
eduferrs <edu.ferr96@gmail.com>
2026-02-11 13:13:30 -0300

    fixing

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,28 +8,26 @@
  */
 function transformStateWithClones(state, actions) {
   let stateCopy = Object.assign({}, state);
-  let statesHistory = [];
+  const statesHistory = [];
 
   for (let i = 0; i < actions.length; i++) {
-    
     switch (actions[i].type) {
       case 'addProperties':
         Object.assign(stateCopy, actions[i].extraData);
-        statesHistory.push(Object.assign({}, stateCopy));
         break;
       case 'removeProperties':
         for (let j = 0; j < actions[i].keysToRemove.length; j++) {
           delete stateCopy[actions[i].keysToRemove[j]];
         }
-        statesHistory.push(Object.assign({}, stateCopy));
         break;
       case 'clear':
         stateCopy = {};
-        statesHistory.push(Object.assign({}, stateCopy));
         break;
       default:
-        break;
+        throw new Error('Unknown action type: ' + actions[i].type);
     }
+
+    statesHistory.push(Object.assign({}, stateCopy));
   }
 
   return statesHistory;
